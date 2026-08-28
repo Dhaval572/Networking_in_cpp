281e2f94680fc7454d510d7ed324b69c659a0d50
Dhaval <dp9718776@gmail.com>
2026-08-28 13:09:05 +0530

    Removed unnecessary things

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@
 int main()
 {
     asio::io_context io;
-    std::cout << "Standalone Asio is working!\n";   
-
-    [[maybe_unused]]int a;
-        
+    std::cout << "Standalone Asio is working!\n";
     return 0;
 }
